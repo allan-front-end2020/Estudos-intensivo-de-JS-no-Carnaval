351e0a6f27fc7a88a41636e162c7bde7fa2f0f42
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-02-26 10:14:37 -0300

    desafio troca de variaveis realizada

---
+++ estudos-jscarnaval/index.js
@@ -0,0 +1,16 @@
+// variavel do tipó string
+var texto = 'Olá mundo'
+
+//variavel do tipo number
+var numeroInteiro = "7" //Int numero inteiro
+var numeroFracionario = '2.3' // float numero fracionario
+
+// variavel do tipo boolean
+var condicao = false
+var condicao1 = true
+
+var nome = prompt('digite saeu nome')
+var idade = prompt('digite sua idade')
+
+document.write('<h1>Olá ' + nome + ' tudo bem ? vimos que vc tem ' + idade + ' de idade </h1>')
+alert(condicao1)
