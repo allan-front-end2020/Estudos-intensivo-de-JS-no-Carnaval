6a98587d1700cae2e4277e75fbd5892305a30a58
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-02-27 13:00:54 -0300

    criada pasta FUnçoes e feito uma pequena aplição que calcula area do terreno

--- desafio faixa de idade/desafio.js
+++ desafio faixa de idade/desafio.js
@@ -1,14 +1,7 @@
- var idade = prompt('digite sua idade')
+function calcularAreTerreno(largura, comprimento) {
 
- if (idade <= 15) {
-     alert('voce ainda e uma criança e tem muitop a viver')
- }
- if (idade >= 15 && idade < 30) {
-     alert('voce e jovem')
- }
- if (idade >= 30 && idade < 60) {
-     alert('voce e  ja esta ficando velho , a morte está proxima')
- }
- if (idade > 60) {
-     alert('Seus dias estão contados aproveite o maximo que puder')
- }
+    var area = largura * comprimento
+    document.write('Terreno possui ' + area + ' de area total')
+
+}
+calcularAreTerreno(20, 30)
---
+++ funçoes/funcoes.js
@@ -0,0 +1,13 @@
+ function tamanhoTerreno(largura, comprimento) {
+
+
+     var largura = prompt('digite a largura do terreno: ')
+     var comprimento = prompt('Digite a largura do terreno: ')
+
+
+     var area = largura * comprimento
+
+     return area
+ }
+
+ document.write(' O tamanho do terreno e ' + tamanhoTerreno() + ' metros quadrados')
