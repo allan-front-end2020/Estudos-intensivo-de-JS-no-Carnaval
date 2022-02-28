05471a3f8b435ea0926d3622bc91ada8b8ea875e
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-02-28 07:55:46 -0300

    Segundo dia do intensivo JS

---
+++ funçoes/funcoes string/strings.js
@@ -0,0 +1,17 @@
+function exibirArtigo(id, callbackSucesso, callbackErro) {
+
+    if (true) {
+        callbackSucesso('Funçoes de callback js', 'Funções de call beck sao muito utilizados no mundo Js')
+    } else {
+        callbackErro('erro ao recuperar os dados')
+    }
+}
+var callbackSucesso = function(titulo, descricao) {
+    document.write('<h1>' + titulo + '</h1>')
+    document.write('<hr/>')
+    document.write('<p>' + descricao + '</p>')
+}
+var callbackErro = function(erro) {
+    document.write('Erro' + erro + '')
+}
+exibirArtigo(1, callbackSucesso, callbackErro)
---
+++ funçoes/funçoes anonimas e técnica de wrapper/funcoes_anonimas.js
@@ -0,0 +1,4 @@
+var exibirSaudacao = function(nome) {
+    document.write('Olá ' + nome + ' Você será um grande progrador full stack')
+}
+exibirSaudacao('allan')
