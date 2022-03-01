bff7247570d4c28eb585594fd54f2f88147ae344
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-03-01 14:56:45 -0300

    separdor de numeros e letras feitas com sucesso

---
+++ dom/dom manipilando inputs/dom.js
@@ -0,0 +1,14 @@
+    function distribuirCaracter() {
+        let caracter = document.getElementById('entrada').value
+            document.getElementById('entrada').value = ''
+
+
+        caracter.trim()
+
+        if (caracter == '1' || caracter == '2' || caracter == '3' || caracter == '4' || caracter == '5' || caracter == '6' || caracter == '7' || caracter == '8' || caracter == '9') {
+            document.getElementById('numeros').value = caracter
+        } else {
+            document.getElementById('letras').value = caracter
+        }
+
+    }
