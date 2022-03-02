d42edbd0cf19e579f3ccf686f68bd3c05a998e64
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-03-02 12:09:11 -0300

    arrays

---
+++ array/array-basico/array.js
@@ -0,0 +1,9 @@
+var listaFrutas = Array()
+
+listaFrutas[1] = 'banana'
+listaFrutas[2] = 'goiaba'
+listaFrutas[3] = 'pera'
+listaFrutas[4] = 'uva'
+listaFrutas[5] = 'maça'
+
+document.write('listaFrutas') 
---
+++ dom/dom manipilando estiloss/estilo.js
@@ -0,0 +1,7 @@
+function modificaCor(mudaCor, largura, altura) {
+    document.getElementById('quadrado').style.backgroundColor = mudaCor
+    document.getElementById('quadrado').style.width = largura
+    document.getElementById('quadrado').style.height = altura
+
+
+}
